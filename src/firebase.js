490177ac75ad9b8e7baa07efc591cbd03dca2f84
Chetanjain10 <chetanjainsrs@gmail.com>
2021-07-10 11:42:46 +0530

    singnup

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app"
+import 'firebase/storage'
+import 'firebase/auth'
+import 'firebase/firestore'
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyCk6ZHWSbpjH3O5qCVNmowaCPtSu041EBg",
+        authDomain: "reels-233c7.firebaseapp.com",
+        projectId: "reels-233c7",
+        storageBucket: "reels-233c7.appspot.com",
+        messagingSenderId: "572861859954",
+        appId: "1:572861859954:web:f148b5bc17e42bb3035aeb"
+    }
+)
+export const auth = firebase.auth();
+const firestore = firebase.storage();
+export const database = {
+    users:firebase.collection('users'),
+    getCurrentTimeStamp = firebase.firestore.FieldValue.serverTimestamp
+}
+export const storage = firebase.storage();
