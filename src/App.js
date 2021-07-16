5d502e75c75160d10f0127bf780fc20b7217c506
Chetanjain10 <chetanjainsrs@gmail.com>
2021-07-16 19:46:42 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './Components/Signup';
+import Login from './Components/Login';
+import Feed from './Components/Feed';
 import AuthProvider from './Context/AuthProvider';
+import {BrowseRouter as Router, Switch, Route} from 'react-router-dom'
 function App() {
   return (
-    <AuthProvider>
-      {/* <h1>Hello</h1> */}
-      <Signup />
-    </AuthProvider>
+    // <AuthProvider>
+    //   {/* <h1>Hello</h1> */}
+    //   <Signup />
+    // </AuthProvider>
+    <Router>
+      <AuthProvider>
+      <Switch>
+        <Route exact path='/' component={Feed} />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup}/>
+      </Switch>
+      </AuthProvider>
+    </Router>
   );
 }
 
